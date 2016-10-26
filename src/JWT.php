f49d0a3a27968ddba63ff78ae6c9555da36f1a5b
Dinh Phong <dinhphong.developer@gmail.com>
2016-10-26 23:14:16 +0700

    update exception

--- src/JWT.php
+++ src/JWT.php
@@ -69,38 +69,38 @@ class JWT
         $timestamp = is_null(static::$timestamp) ? time() : static::$timestamp;
 
         if (empty($key)) {
-            throw new InvalidArgumentException('Key may not be empty');
+            return false;
         }
         if (!is_array($allowed_algs)) {
-            throw new InvalidArgumentException('Algorithm not allowed');
+            return false;
         }
         $tks = explode('.', $jwt);
         if (count($tks) != 3) {
-            throw new UnexpectedValueException('Wrong number of segments');
+            return false;
         }
         list($headb64, $bodyb64, $cryptob64) = $tks;
         if (null === ($header = static::jsonDecode(static::urlsafeB64Decode($headb64)))) {
-            throw new UnexpectedValueException('Invalid header encoding');
+            return false;
         }
         if (null === $payload = static::jsonDecode(static::urlsafeB64Decode($bodyb64))) {
-            throw new UnexpectedValueException('Invalid claims encoding');
+            return false;
         }
         $sig = static::urlsafeB64Decode($cryptob64);
         
         if (empty($header->alg)) {
-            throw new UnexpectedValueException('Empty algorithm');
+            return false;
         }
         if (empty(static::$supported_algs[$header->alg])) {
-            throw new UnexpectedValueException('Algorithm not supported');
+            return false;
         }
         if (!in_array($header->alg, $allowed_algs)) {
-            throw new UnexpectedValueException('Algorithm not allowed');
+            return false;
         }
         if (is_array($key) || $key instanceof \ArrayAccess) {
             if (isset($header->kid)) {
                 $key = $key[$header->kid];
             } else {
-                throw new UnexpectedValueException('"kid" empty, unable to lookup correct key');
+                return false;
             }
         }
 
@@ -112,18 +112,22 @@ class JWT
         // Check if the nbf if it is defined. This is the time that the
         // token can actually be used. If it's not yet that time, abort.
         if (isset($payload->nbf) && $payload->nbf > ($timestamp + static::$leeway)) {
-            throw new BeforeValidException(
-                'Cannot handle token prior to ' . date(DateTime::ISO8601, $payload->nbf)
-            );
+            // throw new BeforeValidException(
+            //     'Cannot handle token prior to ' . date(DateTime::ISO8601, $payload->nbf)
+            // );
+
+            return false;
         }
 
         // Check that this token has been created before 'now'. This prevents
         // using tokens that have been created for later use (and haven't
         // correctly used the nbf claim).
         if (isset($payload->iat) && $payload->iat > ($timestamp + static::$leeway)) {
-            throw new BeforeValidException(
-                'Cannot handle token prior to ' . date(DateTime::ISO8601, $payload->iat)
-            );
+            // throw new BeforeValidException(
+            //     'Cannot handle token prior to ' . date(DateTime::ISO8601, $payload->iat)
+            // );
+
+            return false;
         }
 
         // Check if this token has expired.
